e72c94b2ad566e081f6d916737f0b614b475a7d9
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2023-01-24 10:34:53 -0300

    arrumei o botão do menu mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-function clickMenu() {
-    if (menu.style.display == 'flex') {
-        menu.style.display = 'none'
+// pegar o botão do menu
+var menuBar = document.querySelector('#burguer')
+
+// adicionar evento click no butão do menu
+menuBar.addEventListener('click', function() {
+
+    // pegar o menu 
+    let menuMobile = document.querySelector('.cabecalho-menu')
+
+    // verificar se o menu está com a classe mostrar-menu
+    if (menuMobile.classList.contains('mostrar-menu')) {
+        // remover a classe para mostrar o menu
+        menuMobile.classList.remove('mostrar-menu')
+        // colocar o burguer no butão menu
+        menuBar.innerHTML = 'menu'
     } else {
-        menu.style.display = 'flex'
+        // adicionar a classe para esconder o menu
+        menuMobile.classList.add('mostrar-menu')
+        // colocar o x no butão menu
+        menuBar.innerHTML = 'close'
     }
-}
+
+    // esconder o menu quando clicar em qualque opção
+    menuMobile.addEventListener('click', function() {
+        // remover a classe para mostrar o menu
+        menuMobile.classList.remove('mostrar-menu')
+        // colocar o burguer no butão menu
+        menuBar.innerHTML = 'menu'
+    })
+})
