a5fd528e46fa6f3f8016a7856524a406831e635e
Rafael Brandão da Silva <113553591+RafaelBrandaoS@users.noreply.github.com>
2023-01-24 18:42:40 -0300

    adicionei funcionalidade aos botões dos projetos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* menu */
 // pegar o botão do menu
 var menuBar = document.querySelector('#burguer')
 
@@ -27,4 +28,110 @@ menuBar.addEventListener('click', function() {
         // colocar o burguer no butão menu
         menuBar.innerHTML = 'menu'
     })
+})
+
+/* mostrar mais sobre projetos */
+// projeto android
+var projetoAndroid = document.getElementById('p-android')
+
+projetoAndroid.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-android')
+    let close = document.getElementById('projeto-android-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
+})
+
+// projeto cordel
+var projetoCordel = document.getElementById('p-cordel')
+
+projetoCordel.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-cordel')
+    let close = document.getElementById('projeto-cordel-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
+})
+
+// projeto social
+var projetoSocial = document.getElementById('p-social')
+
+projetoSocial.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-social')
+    let close = document.getElementById('projeto-social-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
+})
+
+// projeto hora
+var projetoHora = document.getElementById('p-hora')
+
+projetoHora.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-hora')
+    let close = document.getElementById('projeto-hora-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
+})
+
+// projeto idade
+var projetoIdade = document.getElementById('p-idade')
+
+projetoIdade.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-idade')
+    let close = document.getElementById('projeto-idade-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
+})
+
+// projeto calculadora
+var projetoCalculadora = document.getElementById('p-calculadora')
+
+projetoCalculadora.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-calculadora')
+    let close = document.getElementById('projeto-calculadora-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
+})
+
+// projeto lista
+var projetoLista = document.getElementById('p-lista')
+
+projetoLista.addEventListener('click', function() {
+    let mais = document.getElementById('projeto-lista')
+    let close = document.getElementById('projeto-lista-close')
+
+    mais.classList.add('ativar')
+
+    close.addEventListener('click', function() {
+        mais.classList.remove('ativar')
+    })
+
 })
